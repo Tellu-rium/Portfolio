7bc9275dcc59e0c8a8098f1ff9205b4bbc1013cf
SUMEET . <officialsumeet22@gmail.com>
2026-06-13 18:46:50 +0530

    smoothen out the animation

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -162,14 +162,14 @@ function ThemeButton({ isLight, setIsLight }) {
       document.documentElement.animate(
         {
           clipPath: [
-            `circle(${endRadius}px at ${x}px ${y}px)`,
             `circle(0px at ${x}px ${y}px)`,
+            `circle(${endRadius}px at ${x}px ${y}px)`,
           ],
         },
         {
           duration: 600,
-          easing: "ease-in-out",
-          pseudoElement: "::view-transition-old(root)",
+          easing: "ease-out",
+          pseudoElement: "::view-transition-new(root)",
         }
       );
     });
